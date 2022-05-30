fafdedf0152ada5062070101d0ed5d08940f791a
Sahil Bhatiani <sahilbhatiani28@gmail.com>
2022-05-31 01:28:44 +1200

    Navbar created

--- bloggy/pages/_app.js
+++ bloggy/pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Navbar from '../components/Navbar'
+import { Toaster } from 'react-hot-toast'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar></Navbar>
+      <Component {...pageProps} />
+      <Toaster />
+    </>
+  )
 }
 
 export default MyApp
--- bloggy/pages/index.js
+++ bloggy/pages/index.js
@@ -2,16 +2,15 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
+import Loader from '../components/Loader';
+import toast from 'react-hot-toast'
 
 export default function Home() {
   return (
     <div>
-      <Link prefetch={false} href = {{
-          pathname: '/[username]',
-          query: {username: 'sahil'}
-        }}>
-          <a>Sahil's Profile</a>
-      </Link>
+      <button onClick={() => toast.success('hello toast!')}>
+        Toast Me
+      </button>
     </div>
-  )
+  );
 }
